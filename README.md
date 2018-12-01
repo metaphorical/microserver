00f50df95785cb018a84bc73b14a9cc58c920ebf
Rastko Vukašinović <rastko.vukasinovic@gmail.com>
2018-12-02 00:11:06 +0100

    Basic http server

---
+++ README.md
@@ -0,0 +1,15 @@
+# Microserver
+
+In pursuit for lightest microservice I am doing experiments in few languages and additionally few approaches within them.
+
+At start notes here will be my own quick drops for myself... I hope to extend as myreasearch progresses
+
+## Erlang
+
+### Basic
+
+Just writing bare server in erlang - http server.
+
+### Psycho
+
+### Cowboy
---
+++ erl_basic/bareserver.erl
@@ -0,0 +1,22 @@
+-module(bareserver).
+-export([start/0]).
+
+start() ->
+    spawn(fun () -> {ok, Sock} = gen_tcp:listen(8000, [{active, false}]), 
+    loop(Sock) end).
+
+loop(Sock) ->
+    {ok, Conn} = gen_tcp:accept(Sock),
+    Handler = spawn(fun () -> handle(Conn) end),
+    gen_tcp:controlling_process(Conn, Handler),
+    loop(Sock).
+
+handle(Conn) ->
+    gen_tcp:send(Conn, response("Hello World")).
+
+response(Str) ->
+    B = iolist_to_binary(Str),
+    iolist_to_binary(
+      io_lib:fwrite(
+         "HTTP/1.0 200 OK\nContent-Type: text/html\nContent-Length: ~p\n\n~s",
+         [size(B), B])).
