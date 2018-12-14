465a434fa0d4b0eb60687273a29c7f900bff6ce6
Rastko <rastko.vukasinovic@gmail.com>
2018-12-14 13:59:11 +0100

    towards rest

--- erl_cowboy/src/id_handler.erl
+++ erl_cowboy/src/id_handler.erl
@@ -1,7 +1,9 @@
 -module(id_handler).
 -behavior(cowboy_handler).
 
--export([init/2]).
+-export([init/2,
+        allowed_methods/0
+        ]).
 
 % First match confirms that method is, in deed get, because this is GET only route
 init(Req0=#{method := <<"GET">>}, State) ->
@@ -9,12 +11,20 @@ init(Req0=#{method := <<"GET">>}, State) ->
     Req = cowboy_req:reply(200, #{
         <<"content-type">> => <<"application/json">>
     }, <<"{\"id\": \"", Id/binary, "\"}">> , Req0),
-    {ok, Req, State};
+    {cowboy_rest, Req, State};
 
+init(Req0=#{method := <<"OPTIONS">>}, State) ->
+    Req = cowboy_req:reply(200, #{
+        <<"allow">> => <<"GET">>
+    }, allowed_methods(), Req0),
+    {cowboy_rest, Req, State};
 
-% Anything not GET goes here
+% Anything not GET or OPTIONS goes here
 init(Req0, State) ->
     Req = cowboy_req:reply(405, #{
         <<"allow">> => <<"GET">>
     }, Req0),
-    {ok, Req, State}.
+    {cowboy_rest, Req, State}.
+
+allowed_methods() -> <<"[GET, POST, DELETE]">>.
+
