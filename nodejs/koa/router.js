ae1be27847b8cf035660384f01ee3839ab6cf050
Rastko Vukašinović <rastko.vukasinovic@gmail.com>
2019-01-11 02:46:39 +0100

    preping

--- nodejs/koa/router.js
+++ nodejs/koa/router.js
@@ -10,6 +10,16 @@ router.get('/', async (ctx) => {
 router.get('/healthy', async (ctx) => {
   ctx.set("X-Health", "Awsome");
 })
+router.get('/bgg/hot', async (ctx) => {
+  ctx.body = {
+    message: "Thanks"
+  };
+})
+router.get('/bgg/item/:id', async (ctx) => {
+  ctx.body = {
+    message: "AJDII"
+  };
+})
 router.get('/:id', async (ctx) => {
   ctx.body = {
     id: ctx.params.id
