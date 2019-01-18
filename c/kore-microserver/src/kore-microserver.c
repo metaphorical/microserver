a549724a344a1fa0adc962b0e4f87d2cfef873fd
Rastko <rastko.vukasinovic@gmail.com>
2019-01-18 23:44:11 +0100

    will get back to c later

--- c/kore-microserver/src/kore-microserver.c
+++ c/kore-microserver/src/kore-microserver.c
@@ -3,6 +3,7 @@
 
 int		root_handler(struct http_request *);
 int		health_handler(struct http_request *);
+int		bgg_hot_handler(struct http_request *);
 
 int
 root_handler(struct http_request *req)
@@ -17,6 +18,15 @@ health_handler(struct http_request *req)
 {
 	char *health_response = "{ \"message\": \"everything is awsome (tm)\"}";
 	http_response_header(req, "X-Health", "Awsome");
+	http_response(req, 200, health_response, strlen(health_response));
+	return (KORE_RESULT_OK);
+}
+
+int
+bgg_hot_handler(struct http_request *req)
+{
+	char *health_response = "{ \"message\": \"hot bgg response goes here\"}";
+	http_response_header(req, "content-type", "application/json");
 	http_response(req, 200, health_response, strlen(health_response));
 	return (KORE_RESULT_OK);
 }
